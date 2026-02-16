8cd48894f2b1d16bae5088cb9880a4a97d0a4b2b
AxelDevMS <asmtel2001@gmail.com>
2026-02-16 16:20:51 -0600

    feat:create project baber-shop

---
+++ src/test/java/ams/dev/api/barber_shop/BarberShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ams.dev.api.barber_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
