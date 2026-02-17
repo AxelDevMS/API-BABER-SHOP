a2438ed53dd5537b142a5cb0c3ea948a1acd6d31
AxelDevMS <asmtel2001@gmail.com>
2026-02-16 21:14:07 -0600

    feat: se crean controladores para exponer enpoints

--- src/test/java/ams/dev/api/barber_shop/BarberShopApplicationTests.java
+++ src/test/java/ams/dev/api/barber_shop/BarberShopApplicationTests.java
@@ -2,8 +2,10 @@ package ams.dev.api.barber_shop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BarberShopApplicationTests {
 
 	@Test
