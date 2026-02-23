513ad86543ef5ae1909cebe91d14ec3c46cddd3b
AxelDevMS <asmtel2001@gmail.com>
2026-02-22 20:28:46 -0600

    fix:ajustes correo al contratar el servicio

--- src/main/java/ams/dev/api/barber_shop/service/EmailService.java
+++ src/main/java/ams/dev/api/barber_shop/service/EmailService.java
@@ -1,7 +1,5 @@
 package ams.dev.api.barber_shop.service;
 
-import ams.dev.api.barber_shop.dto.MailDataDto;
-
 public interface EmailService {
 
     void sendEmailCredentialsAcces(String toEmail, String nombre, String username, String password);
