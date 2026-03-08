49f18d33514457ac31bedd2e6809a33b2789047e
AxelDevMS <asmtel2001@gmail.com>
2026-03-08 17:06:57 -0600

    fix: update route

--- src/main/java/ams/dev/api/barber_shop/controller/BarberShopController.java
+++ src/main/java/ams/dev/api/barber_shop/controller/BarberShopController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/shop")
+@RequestMapping("/api/shop")
 public class BarberShopController {
 
     @Autowired
